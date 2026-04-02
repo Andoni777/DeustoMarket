e0b455e4e42e745b719462b1c90f42bcd79b9931
Andoni Gonzalez <andoni.g@opendeusto.es>
2026-04-02 14:42:13 +0200

    Base de datos y funcion de mostrar supermercados implementada

--- menuAdmin/menuGestSupermercado/menuGestSupermercado.c
+++ menuAdmin/menuGestSupermercado/menuGestSupermercado.c
@@ -7,6 +7,7 @@
 
 #include "menuGestSupermercado.h"
 #include "../menuAdmin.h"
+#include "../../BDD/sqlite3.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -38,6 +39,35 @@ char mostrarMenuGestSuper() {
 
 }
 
+void mostrarListadoSuper (){
+	sqlite3 *db;
+	sqlite3_stmt *stmt;
+	int result;
+
+	sqlite3_open("BDD/deustomarket.db", &db);
+
+	/* --- SELECT --- */
+	char sql[] = "select s.nombre_super from supermercado s";
+
+	sqlite3_prepare_v2(db, sql, strlen(sql), &stmt, NULL) ;
+
+	printf("\n");
+	do {
+		result = sqlite3_step(stmt) ;
+		if (result == SQLITE_ROW) {
+			printf("%s\n", (char*) sqlite3_column_text(stmt, 0));
+		}
+	} while (result == SQLITE_ROW);
+	printf("\n");
+
+	sqlite3_finalize(stmt);
+
+	/* --- SELECT (fin) --- */
+
+	sqlite3_close(db);
+
+}
+
 void menuGestSupermercado() {
 
 	char opcion;
@@ -59,10 +89,18 @@ void menuGestSupermercado() {
 				break;
 
 			case '4':
-				printf("opcion 4");
-				break;
+				printf("\nMostrando supermercados:\n");
+				    mostrarListadoSuper();
+
+				    printf("--> Pulsa ENTER para volver al menu...");
+				    fflush(stdout);
+
+				    getchar();
+				    break;
 
 		}
 	}while(opcion != '0');
 	menuAdmin(); // Vuelve al menu principal
 }
+
+
